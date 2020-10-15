65e86c54f984e735a9db1ca37f69d52cf88c09fc
FedoseevaAlexandra <70970063+FedoseevaAlexandra@users.noreply.github.com>
2020-10-15 09:17:26 +0300

    Update problema_1.py

--- problema_1.py
+++ problema_1.py
@@ -8,7 +8,7 @@ if (p1>p2) and (p1>p3):
     print('punctaj maxim are elevul nr',n1)
 if (p2>p1) and (p2>p3):
     print('punctaj maxim are elevul nr',n2)
-if (p3>p2) and (p3>p31): 
+if (p3>p2) and (p3>p1): 
     print('punctaj maxim are elevul nr',n3)  
 if (p1==p2) and (p1>p3) and (p2>p3):
     print(n1,n2,'au acelas punctaj')
